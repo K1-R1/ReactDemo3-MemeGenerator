dc3503112dc3c542820438acc2c156cb0002e42b
K1-R1 <kyranrawlinson@gmail.com>
2022-03-13 21:21:06 +0000

    Added and setup header component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+import Header from "./components/Header"
+import "./style.css"
+
 export default function App() {
-    return <h1>Hello world!</h1>
+    return (
+        <div>
+            <Header />
+        </div>
+    )
 }
