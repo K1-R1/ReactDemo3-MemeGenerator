9d392ffc57a16d55cebed885daf58da267781f8d
K1-R1 <kyranrawlinson@gmail.com>
2022-03-13 21:35:31 +0000

    Created and added Meme component

---
+++ src/components/Meme.js
@@ -0,0 +1,19 @@
+export default function Meme() {
+    return (
+        <main>
+            <form className="form">
+                <input
+                    type="text"
+                    className="form--input"
+                    placeholder="Top text"
+                />
+                <input
+                    type="text"
+                    className="form--input"
+                    placeholder="Top text"
+                />
+                <button className="form--button">Get a new meme image</button>
+            </form>
+        </main>
+    )
+}
